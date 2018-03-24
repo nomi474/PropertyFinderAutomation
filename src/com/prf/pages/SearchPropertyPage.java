0e260df72ea3555a655ee25feb1e0078cdec32ae
Nauman <nomi474@yahoo.com>
2018-03-24 06:25:54 +0400

    Deleted old versions of selenium jar files

--- src/com/prf/pages/SearchPropertyPage.java
+++ src/com/prf/pages/SearchPropertyPage.java
@@ -7,12 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.prf.common.CommonFeatures;
 
